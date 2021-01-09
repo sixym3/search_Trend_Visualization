f608cc7da524361acdce51f67de33b586e2c6fd2
Eric Xiao <eric_xiao@Erics-MacBook-Pro.local>
2021-01-08 19:07:57 -0500

    Added a csv function to store data pulled, so that historical data does not have to be pulled everytime the function is ran

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import pandas_datareader.data as dr
 import datetime
+import numpy as np
 import matplotlib.pyplot as plt
 from pytrends.request import TrendReq
 from pytrends.dailydata import get_daily_data
 
 pd.options.display.float_format = '{:,.1f}'.format
 
-start = datetime.datetime(2020, 12, 1) # start date
+start = datetime.datetime(2020, 11, 1) # start date
 end = datetime.date.today() # end date
+csv = "test"
+db = pd.DataFrame(np.array([["BTC-USD", "bitcoin"]]), columns=["sign", "search"])
 
 def ticker_vs_time(ticker, start, end):
     stock = dr.DataReader(ticker, "yahoo", start, end) # get stock data
@@ -18,8 +21,18 @@ def ticker_vs_time(ticker, start, end):
     fig.set_size_inches(20, 3)
     plt.show()
 
+def update_interest_over_time(ticker, start, end):
+    #define a different start date for update function
+    get_daily_data(ticker, start.year, start.month, end.year, end.month, geo = "US", verbose=True).to_csv(csv)
+
 def plot_interest_over_time(ticker, start, end):
-    pytrend = get_daily_data("bitcoin", start.year, start.month, end.year, end.month, geo = "US", verbose=True)
+    pytrend = pd.read_csv(csv)
+    plt.plot(pytrend.index, pytrend["bitcoin"])
+    plt.title("Interest Over Time")
+    fig = plt.gcf()
+    fig.set_size_inches(20, 3)
+    plt.show()
+
     """
     pytrend = Trendreq()
     pytrend.build_payload(kw_list=[ticker], # len(kw_list) <= 5
@@ -28,13 +41,7 @@ def plot_interest_over_time(ticker, start, end):
     interest_over_time_df = pytrend.interest_over_time()
     plt.plot(interest_over_time_df.index, interest_over_time_df[ticker])
     """
-    print(pytrend)
-    plt.plot(pytrend.index, pytrend["bitcoin"])
-    plt.title("Interest Over Time")
-    fig = plt.gcf()
-    fig.set_size_inches(20, 3)
-    plt.show()
-    plt.savefig("Interest_chart")
+
 
 def interest_and_price_over_time(ticker, start, end):
     fig, ax1 = plt.subplots()
@@ -62,7 +69,8 @@ def interest_and_price_over_time(ticker, start, end):
     plt.savefig("BTC-USD")
 
 #plot_interest_over_time("BTC/USD", start, end)
-plot_interest_over_time("BTC-USD", start, end)
+update_interest_over_time(db["search"][0], start, end)
+plot_interest_over_time(db["sign"][0], start, end)
 
 #interest_and_price_over_time("BTC-USD", start, end)
 
